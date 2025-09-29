f4a2ff043943c4280e6e6640dadd96e517e43f8e
Mohammad-Arslan <69963409+Mohammad-Arslan@users.noreply.github.com>
2025-09-29 11:51:32 +0500

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -103,7 +103,7 @@ export default function About() {
                 }}>
                 <Mail className="mr-2 h-4 w-4" /> Contact Me
               </Button>
-              <a href="/files/mohammad-arslan-resume.pdf" download="Mohammad_Arslan_Resume.pdf">
+              <a href="/mohammad-arslan-resume.pdf" download="Mohammad_Arslan_Resume.pdf">
                 <Button
                   variant="outline"
                   className="border-teal-500 text-teal-500 hover:bg-teal-500/10 flex items-center"
